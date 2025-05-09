a6213f4dc4bc1a4f3a932104ff378ae31cd3db7e
Arthur Danilin <mohnatuy0@gmail.com>
2025-05-09 19:54:27 +0300

    new update

--- backend_api/app/app_factory.py
+++ backend_api/app/app_factory.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from applications.users.router import router_users
+from app.applications.users.router import router_users
 
 
 def get_application() -> FastAPI:
--- backend_api/app/applications/users/router.py
+++ backend_api/app/applications/users/router.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter, status
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from applications.users.schemas import BaseFields, RegisterUserFields
+from app.applications.users.schemas import BaseFields, RegisterUserFields
 
 router_users = APIRouter()
 
 
 @router_users.post("/create", status_code=status.HTTP_201_CREATED)
-async def create_user(new_user: RegisterUserFields) -> BaseFields:
+async def create_user(new_user: RegisterUserFields, session: AsyncSession) -> BaseFields:
     return new_user
