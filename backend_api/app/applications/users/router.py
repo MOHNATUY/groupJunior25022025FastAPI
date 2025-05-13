989534fa68b8bfa9cf1ba191584afe265b7f71a4
Arthur Danilin <mohnatuy0@gmail.com>
2025-05-13 20:44:51 +0300

    new update

--- backend_api/app/applications/users/router.py
+++ backend_api/app/applications/users/router.py
@@ -11,7 +11,7 @@ router_users = APIRouter()
 
 async def create_user_in_db(email, name, password, session: AsyncSession):
     hashed_password = await PasswordEncrypt.get_password_hash(password)
-    new_user = User(email=email, hashed_password=PasswordEncrypt.get_password_hash(password), name=name)
+    new_user = User(email=email, hashed_password=hashed_password, name=name)
     session.add(new_user)
     await session.commit()
 
