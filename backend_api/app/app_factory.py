0d34ed6635cb5b6292cd1ef46c0a1e6be331693e
Arthur Danilin <mohnatuy0@gmail.com>
2025-05-16 19:20:11 +0300

    add router auth

--- backend_api/app/app_factory.py
+++ backend_api/app/app_factory.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 
 from settings import settings
+from applications.auth.router import router_auth
 from applications.users.router import router_users
 
 
 def get_application() -> FastAPI:
     app = FastAPI(root_path="/api", root_path_in_servers=True, debug=settings.DEBUG)
     app.include_router(router_users, prefix="/users", tags=["Users"])
-
+    app.include_router(router_users, prefix="/auth", tags=["Auth"])
     return app
