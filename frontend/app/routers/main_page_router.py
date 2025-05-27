1ebe19546a6ab29c51b06eb391121d7c07df5fa5
Arthur Danilin <mohnatuy0@gmail.com>
2025-05-27 20:15:43 +0300

    new update

--- frontend/app/routers/main_page_router.py
+++ frontend/app/routers/main_page_router.py
@@ -12,3 +12,9 @@ async def index(request: Request):
     context = {'request': request, 'data': 123}
     response = templates.TemplateResponse('index.html', context=context)
     return response
+
+@router.get('/login')
+async def login(request: Request):
+    context = {'request': request, 'data': 123}
+    response = templates.TemplateResponse('index.html', context=context)
+    return response
