0e11d42ba752490e0fa049bd3d20cf68494e2cce
Arthur Danilin <mohnatuy0@gmail.com>
2025-05-30 21:01:43 +0300

    new update

--- frontend/app/routers/main_page_router.py
+++ frontend/app/routers/main_page_router.py
@@ -7,6 +7,7 @@ router = APIRouter()
 
 templates = Jinja2Templates(directory='templates')
 
+
 async def get_current_user_with_token(request: Request) -> dict:
     access_token = request.cookies.get('access_token')
     if not access_token:
@@ -16,6 +17,7 @@ async def get_current_user_with_token(request: Request) -> dict:
     return user
 
 
+
 @router.get('/')
 async def index(request: Request, user: dict=Depends(get_current_user_with_token)):
     context = {'request': request}
@@ -23,6 +25,7 @@ async def index(request: Request, user: dict=Depends(get_current_user_with_token
         context['user'] = user
     response = templates.TemplateResponse('index.html', context=context)
     return response
+
 
 async def login_user(user_email: str, password: str):
     async with httpx.AsyncClient() as client:
@@ -50,10 +53,9 @@ async def get_user_info(access_token: str):
 @router.get('/login')
 @router.post('/login')
 async def login(request: Request, user: dict=Depends(get_current_user_with_token), user_email: str = Form(''), password: str = Form('')):
-    context = {'request': request}
-    print(user, 55555555555555555555555)
+    context = {'request': request, "entered_email": user_email}
+    redirect_url = request.url_for("index")
     if user.get('name'):
-        redirect_url = request.url_for("index")
         response = RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
         return response
 
@@ -65,10 +67,17 @@ async def login(request: Request, user: dict=Depends(get_current_user_with_token
     user_tokens = await login_user(user_email, password)
     access_token = user_tokens.get('access_token')
     if not access_token:
+        errors = ["Incorrect login or password"]
+        context['errors'] = errors
         return templates.TemplateResponse('login.html', context=context)
 
-    user = await get_user_info(access_token)
-    context["user"] = user
-    response = templates.TemplateResponse('login.html', context=context)
+    response = RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
     response.set_cookie(key="access_token", value=access_token, httponly=True, max_age=60*5)
+    return response
+
+@router.get('/logout')
+async def logout(request: Request):
+    redirect_url = request.url_for("login")
+    response = RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
+    response.delete_cookie('access_token')
     return response
