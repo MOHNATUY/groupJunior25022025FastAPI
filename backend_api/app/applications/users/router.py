6bac7b44d45d656c6f971f05d7adc6513b83658b
Arthur Danilin <mohnatuy0@gmail.com>
2025-06-10 20:04:20 +0300

    new update

--- backend_api/app/applications/users/router.py
+++ backend_api/app/applications/users/router.py
@@ -6,6 +6,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from applications.users.crud import create_user_in_db, get_user_by_email, activate_user_account
 from applications.users.schemas import BaseUserInfo, RegisterUserFields
 from database.session_dependencies import get_async_session
+from services.rabbit.constants import SupportedQueues
+from services.rabbit.rabbitmq_service import rabbitmq_broker
 
 router_users = APIRouter()
 
@@ -17,12 +19,12 @@ async def create_user(new_user: RegisterUserFields, session: AsyncSession = Depe
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail='Already exists')
 
     created_user = await create_user_in_db(new_user.email, new_user.name, new_user.password, session)
-    # todo send email
+    await rabbitmq_broker.send_message({"ss": "bb"}, SupportedQueues.USER_REGISTRATION)
 
     return created_user
 
 
-@router_users.get("/verify/{user_uuid}")
+@router_users.get('/verify/{user_uuid}')
 async def verify_user(user_uuid: uuid.UUID, session: AsyncSession = Depends(get_async_session)):
     await activate_user_account(user_uuid, session)
     return {"Status": "activated"}
