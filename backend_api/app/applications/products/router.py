9a93b15a4ac34e1b994761e5198f28414bcc45fe
Arthur Danilin <mohnatuy0@gmail.com>
2025-06-17 19:57:59 +0300

    new update

--- backend_api/app/applications/products/router.py
+++ backend_api/app/applications/products/router.py
@@ -2,6 +2,17 @@ from fastapi import APIRouter
 
 products_router = APIRouter()
 
-@products_router.post('')
-async  def create_product():
+
+@products_router.post('/')
+async def create_product():
+    return
+
+
+@products_router.get('/{pk}')
+async def get_product(pk: int):
+    return
+
+
+@products_router.get('/')
+async def get_products():
     return
